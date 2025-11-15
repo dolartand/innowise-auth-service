0c5d0cc2e1537f45ee0d9266cf003596a3219dc8
dolartand <dolartand@gmail.com>
2025-11-16 02:34:08 +0300

    feat: add logic for jwt tokens

---
+++ src/main/java/com/innowise/authservice/security/JwtProps.java
@@ -0,0 +1,29 @@
+package com.innowise.authservice.security;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Getter
+@Setter
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProps {
+
+    @NotBlank(message = "JWT secret is required")
+    private String secret;
+
+    @NotNull(message = "Access token is required")
+    private Long  accessTokenExp;
+
+    @NotNull(message = "Refresh token is required")
+    private Long refreshTokenExp;
+
+    @NotBlank(message = "Issuer is required")
+    private String issuer;
+}
